3e3678a44f9272c35bf94ccd29b8e42fb34b83ad
Emma-F-git <emma.fortat@yahoo.fr>
2025-05-29 17:20:06 +0200

    Modification des routes post et modify suite à l'ajout de multer

--- routes/stuff.js
+++ routes/stuff.js
@@ -1,14 +1,15 @@
 const express = require("express");
-const auth = require("auth");
 const router = express.Router();
+const auth = require("auth");
+const multer = require("../midleware/multer-config");
 
 const stuffCtrl = require("../controllers/stuff");
 
 /*Routes CRUD création, lecture, modification, suppression*/
 router.get("/", auth, stuffCtrl.getAllBooks);
-router.post("/", auth, stuffCtrl.createBook);
+router.post("/", auth, multer, stuffCtrl.createBook);
 router.get("/:id", auth, stuffCtrl.getOneBook);
-router.put("/:id", auth, stuffCtrl.modifyBook);
+router.put("/:id", auth, multer, stuffCtrl.modifyBook);
 router.delete("/:id", auth, stuffCtrl.deleteBook);
 
 module.exports = router;
