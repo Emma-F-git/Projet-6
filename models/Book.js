cf98b7ecb6c00bd2ac70daf966a682b7f4baca03
Emma-F-git <emma.fortat@yahoo.fr>
2025-06-02 14:47:49 +0200

    Ajout du genre dans les modèles book

--- models/Book.js
+++ models/Book.js
@@ -6,6 +6,7 @@ const bookSchema = mongoose.Schema({
   imageUrl: { type: String, required: true },
   author: { type: String, required: true },
   year: { type: Number, required: true },
+  genre: { type: String, required: true },
   ratings: [
     {
       userId: { type: String, required: true },
